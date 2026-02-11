f5209a7f0e89c2610c11b0fd7f7371755cbf939b
path2revenue <bilal@path2revenue.com>
2026-02-11 22:04:00 +0100

    feat: add Meta Pixel tracking (ID: 1109647120733220)

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import { siteConfig } from "@/site.config";
 
 const { meta, design } = siteConfig;
@@ -22,8 +23,8 @@ export const metadata = {
 };
 
 const fontUrl = `https://fonts.googleapis.com/css2?family=${encodeURIComponent(design.fontHeading)}:wght@${design.fontWeights}&display=swap${design.fontBody !== design.fontHeading
-    ? `&family=${encodeURIComponent(design.fontBody)}:wght@${design.fontWeights}&display=swap`
-    : ""
+  ? `&family=${encodeURIComponent(design.fontBody)}:wght@${design.fontWeights}&display=swap`
+  : ""
   }`;
 
 export default function RootLayout({ children }) {
@@ -33,12 +34,20 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href={fontUrl} rel="stylesheet" />
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1109647120733220&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body
         className="antialiased"
         style={{ fontFamily: `'${design.fontBody}', sans-serif` }}
       >
         {children}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1109647120733220');fbq('track','PageView');`}
+        </Script>
       </body>
     </html>
   );
